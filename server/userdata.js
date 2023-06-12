bdc75b7cbb437ec711bcad5e0090d6a9968d9ab3
Nupur Gohite <118847362+gohitenupur@users.noreply.github.com>
2023-06-12 14:07:31 +0530

    upd

--- server/userdata.js
+++
@@ -1,7 +0,0 @@
-let userdata = [
-    {name:"user2", password:"user1", email:"user1@gmail.com"},
-    {name:"rahul", password:"chingchong", email:"rahul@gmail.com"},
-    {name:"user1", password:"user1", email:"user1@gmail.com"}
-]
-
-module.exports = userdata;
